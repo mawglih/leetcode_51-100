e598a72193a111fa7e0db465e3a64ee898a5b9ee
mawglih <mawglih@gmail.com>
2023-05-30 21:44:23 -0400

    first commit

---
+++ leet-51-100/LC53_Maximum_Subarray/index.js
@@ -0,0 +1,14 @@
+/**
+ * Given an integer array nums, find the subarray  with the largest sum, and return its sum.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    
+};
+
+const n  = [-2,1,-3,4,-1,2,1,-5,4];
+console.log(maxSubArray(n))
