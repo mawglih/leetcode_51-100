8c32bd47d4c15056e645b0eb886bb2da4cd0d3ad
mawglih <mawglih@gmail.com>
2023-06-15 15:10:39 -0400

    LC79

--- leet-51-100/LC79_Word_Search/index.js
+++ leet-51-100/LC79_Word_Search/index.js
@@ -20,5 +20,36 @@ Output: false
  * @return {boolean}
  */
 var exist = function(board, word) {
-    
+    const n = board.length;
+    const m = board[0].length;
+    if(word.length < 1) return false;
+    const helper = (i, j, pointer) => {
+        if(i === n || i < 0 || j === m || j < 0 || board[i][j] !== word[pointer]) return false;
+        if(pointer === word.length - 1) return true;
+        board[i][j] = '.';
+        const found = 
+            helper(i + 1, j, pointer + 1) ||
+            helper(i - 1, j, pointer + 1) ||
+            helper(i, j + 1, pointer + 1) ||
+            helper(i, j - 1, pointer + 1);
+        board[i][j] = word[pointer];
+        return found;
+    }
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < m; j++) {
+            if(word[0] === board[i][j]) {
+                const match = helper(i, j, 0);
+                if(match) return true;
+            }
+        }
+    }
+    return false;
 };
+
+
+const b = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]; 
+const w = "SEE"
+console.log(exist(b, w));
+
+// runtime 617 ms
+// memory 42.2 MB
