83fb6b5137125a7abd79b4ee1b2b1ca1963ae10a
mawglih <mawglih@gmail.com>
2023-06-15 22:36:06 -0400

    LC81

--- leet-51-100/LC81_Search_InRotated_sorted_array/index.js
+++ leet-51-100/LC81_Search_InRotated_sorted_array/index.js
@@ -17,5 +17,36 @@ Output: false
  * @return {boolean}
  */
 var search = function(nums, target) {
-    
+    let left = 0;
+    let right = nums.length - 1;
+    while(left <= right) {
+        let mid = Math.floor((right + left) / 2);
+        if(nums[left] === target) return true;
+        if(nums[right] === target) return true;
+        if(nums[mid] === target) return true;
+
+        if(nums[mid] === nums[left]) {
+            left++;
+            continue;
+        }
+        if(nums[mid] > nums[left]) {
+            if(nums[left] < target && nums[mid] > target){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if(nums[right] > target && nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+    return false;
 };
+
+console.log(search([1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1], 2));
+
+// runtime 64 ms
+// memory 42.5 MB
