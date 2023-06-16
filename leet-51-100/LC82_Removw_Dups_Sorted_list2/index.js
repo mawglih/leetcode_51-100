72efb27c2c78c0f1350d9186cffbe7081aeeedad
mawglih <mawglih@gmail.com>
2023-06-15 20:06:25 -0400

    added 81-100 description

---
+++ leet-51-100/LC82_Removw_Dups_Sorted_list2/index.js
@@ -0,0 +1,22 @@
+/**
+ * Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+ * 
+ * Example
+ * Input: head = [1,2,3,3,4,4,5]
+    Output: [1,2,5]
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    
+};
---
+++ leet-51-100/LC90_Subsets2/index.js
@@ -0,0 +1,19 @@
+/**
+ * Given an integer array nums that may contain duplicates, return all possible subsets (the power set). The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+
+Example 2:
+Input: nums = [0]
+Output: [[],[0]]
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    
+};
