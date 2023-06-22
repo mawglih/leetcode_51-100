b0eaf00f11d657979cc410bab68f139ef5e2ad3c
mawglih <mawglih@gmail.com>
2023-06-21 22:02:09 -0400

    LC85

--- leet-51-100/LC85_Maximal_rectangle/index.js
+++ leet-51-100/LC85_Maximal_rectangle/index.js
@@ -19,6 +19,35 @@ Output: 1
  * @param {character[][]} matrix
  * @return {number}
  */
-var maximalRectangle = function(matrix) {
-    
-};
+const maximalRectangle = function(matrix) {
+    if (!matrix.length) return 0;
+    const rows = matrix.length;
+    const columns = matrix[0].length;
+    const newArr = new Array(rows).fill(0).map(cell => new Array(columns).fill(0));
+    let maxArea = 0;
+    console.log('new arr', newArr);
+    console.log('arr', arr);
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < columns; col++) {
+        if(row === 0) {
+            newArr[row][col] = matrix[row][col] == '1' ? 1 : 0;
+        } else {
+            newArr[row][col] = matrix[row][col] == '1' ? (newArr[row - 1][col] + 1) : 0;
+        }
+        let minHeight = newArr[row][col];
+        for(let pointer = col; pointer >= 0; pointer--) {
+           if (minHeight === 0) break;
+           if (newArr[row][pointer] < minHeight) minHeight = newArr[row][pointer];
+           maxArea = Math.max(maxArea, minHeight * (col - pointer + 1));
+        }
+     }
+    }
+    console.log('new arr2', newArr);
+    return maxArea;  
+  };
+
+  const arr = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]];
+  console.log(maximalRectangle(arr));
+  
+  //runtime 88 ms
+  //memory 44.4. MB
