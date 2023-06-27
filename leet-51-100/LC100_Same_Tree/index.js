e23af1a7e533b3576af473e8e90350c95a9c38c0
mawglih <mawglih@gmail.com>
2023-06-26 20:37:17 -0400

    LC100

--- leet-51-100/LC100_Same_Tree/index.js
+++ leet-51-100/LC100_Same_Tree/index.js
@@ -28,5 +28,7 @@ Output: false
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    
+    if (!p && !q) return true;
+    if (!p || !q || p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
